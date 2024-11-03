9947a6e758b9d019de6b64e7827dc433743352ac
eunbb <fakesilverpark@gmail.com>
2024-11-04 02:48:28 +0900

    cors 오류 해결

--- src/main/java/com/tabularasa/unexpectedstories/config/CorsConfig.java
+++ src/main/java/com/tabularasa/unexpectedstories/config/CorsConfig.java
@@ -10,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("http://client");
+                .allowedOrigins("http://localhost:3000") // 허용할 도메인 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // 허용할 HTTP 메소드 설정
+                .allowedHeaders("*") // 허용할 헤더 설정
+                .allowCredentials(true) // 인증정보 허용 여부
+                .maxAge(3600); // preflight 요청의 유효시간 설정
     }
 }
