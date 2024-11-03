1451955876ad5b57bce8fca50f3a32b1feb72592
eunbb <fakesilverpark@gmail.com>
2024-11-04 03:40:11 +0900

    cors 오류 해결3

--- src/main/java/com/tabularasa/unexpectedstories/config/WebConfig.java
+++ src/main/java/com/tabularasa/unexpectedstories/config/WebConfig.java
@@ -5,15 +5,16 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 모든 도메인 허용
+                .allowedOrigins("http://localhost:3001", "http://10.150.151.211:3000") // 허용할 도메인들 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드 설정
                 .allowedHeaders("*") // 허용할 헤더 설정
                 .allowCredentials(true) // 인증정보 허용 여부
                 .maxAge(3600); // preflight 요청의 유효시간 설정
     }
 }
+
